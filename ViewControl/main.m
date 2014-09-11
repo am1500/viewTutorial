78e6dd0705ac465cabded01dd27366f6a07a5e1b
am1500 <carlilessard@wl-208-27.wpa.claremont.edu>
2014-09-10 17:06:57 -0700

    Initial Commit

---
+++ ViewControl/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewControl
+//
+//  Created by Carli Lessard on 9/10/14.
+//  Copyright (c) 2014 Alejandro Mendoza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AMAppDelegate class]));
+    }
+}
---
+++ ViewControlTests/ViewControlTests.m
@@ -0,0 +1,34 @@
+//
+//  ViewControlTests.m
+//  ViewControlTests
+//
+//  Created by Carli Lessard on 9/10/14.
+//  Copyright (c) 2014 Alejandro Mendoza. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ViewControlTests : XCTestCase
+
+@end
+
+@implementation ViewControlTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
